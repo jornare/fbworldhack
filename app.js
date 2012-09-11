c76e2e5347f95aca382732dccacb095fa6af783f
Jørn Are Hatlelid <jah@computas.com>
2012-09-11 16:19:25 +0200

    bugfix

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , io = require('socket.io');
 
 var app = express();
 
@@ -33,11 +34,12 @@ app.get('/albums', routes.albums);
 app.get('/pictureviewer', routes.pictureviewer);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app);
+server.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
 
-
+io = io.listen(server);
 //events
 io.sockets.on('connection', function (socket) {
 	  //socket.emit('news', { hello: 'world' });
